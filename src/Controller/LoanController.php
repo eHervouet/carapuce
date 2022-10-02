a8361f7ab0d34336189221c1f70fdcdda873c7c3
SamouelS <42437491+SamouelS@users.noreply.github.com>
2022-10-02 20:45:38 +0200

    Samuel gestion emprunts + remove passenger

--- src/Controller/LoanController.php
+++ src/Controller/LoanController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Repository\LoanRepository;
-use App\Repository\PersonRepository;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
-use App\Form\Type\LoanType;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Loan;
 use App\Entity\Passenger;
+use App\Form\Type\LoanType;
+use App\Repository\LoanRepository;
+use App\Repository\PersonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Collections\Criteria;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/loan', name: 'loan_')]
 class LoanController extends AbstractController
@@ -20,13 +21,13 @@ class LoanController extends AbstractController
 
     #[Route('/list', name: 'list')]
     public function index(LoanRepository $loanRepository): Response
-    {
+    {      
         $this->denyAccessUnlessGranted('ROLE_USER');
-
-        $listLoans = $loanRepository->findAll();
         
+        $listLoans = $loanRepository->listerTrajets();
         return $this->render('loan/list.html.twig', [
-            "loans" =>$listLoans
+            "loans" =>$listLoans,
+            "title" => "Liste des trajets"
         ]);
     }
 
@@ -35,10 +36,23 @@ class LoanController extends AbstractController
     {
         $this->denyAccessUnlessGranted('ROLE_USER');
         $user = $this->getUser();
-        $listLoans = $loanRepository->findBy(['driver' => $user->getId()]);
+        $listLoans = $loanRepository->findBy(['driver' => $user->getId()],array('departDate' => 'DESC'));
         
-        return $this->render('loan/list.html.twig', [
-            "loans" =>$listLoans
+        return $this->render('loan/myList.html.twig', [
+            "loans" =>$listLoans,
+            "title" => "Mes emprunts"
+        ]);
+    }
+
+    #[Route('/gestion', name: 'gestion')]
+    public function loansValidation(LoanRepository $loanRepository): Response
+    {      
+        $this->denyAccessUnlessGranted('ROLE_GESTIONNAIRE');
+
+        $listLoans = $loanRepository->findBy(array(),array('departDate' => 'DESC'));
+        return $this->render('loan/gestion.html.twig', [
+            "loans" =>$listLoans,
+            "title" => "Validations des emprunts"
         ]);
     }
     
@@ -63,7 +77,7 @@ class LoanController extends AbstractController
             $entityManager->flush();
 
             $this->addFlash('success','Demande d\'emprunt créée');
-            return $this->redirectToRoute('loan_list');
+            return $this->redirectToRoute('loan_maListe');
         }
 
         return $this->render('loan/add.html.twig', [
@@ -86,22 +100,37 @@ class LoanController extends AbstractController
     public function valider(int $id, EntityManagerInterface $entityManager, Request $request, LoanRepository $loanRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_GESTIONNAIRE');
-
         $loan = $loanRepository->find($id);
-        $loan->setStatut("Validé");
 
+        if($loan->getStatut() != "En validation" ){
+            $this->addFlash(
+                'danger',
+                'L\'emprunt doit être en validation !'
+            );
+            return $this->redirectToRoute('loan_gestion');
+        }
+        $loan->setStatut("Validé");
         $entityManager->persist($loan);
         $entityManager->flush();
         
-        return $this->redirectToRoute('loan_list');
+        return $this->redirectToRoute('loan_gestion');
     }
 
     #[Route('/refuser/{id}', name: 'refuser')]
     public function refuser(int $id, EntityManagerInterface $entityManager, Request $request, LoanRepository $loanRepository): Response
     {
         $this->denyAccessUnlessGranted('ROLE_GESTIONNAIRE');
-
         $loan = $loanRepository->find($id);
+        $now = new \DateTime("now");
+
+        if($loan->getStatut() != "En validation" && $loan->getStatut() != "Validé" && $loan->getDepartDate() < $now ){
+            $this->addFlash(
+                'danger',
+                'L\'emprunt doit être en validation ou validé et ne doit pas être en cours !'
+            );
+            return $this->redirectToRoute('loan_gestion');
+        }
+
         $loan->setReturnVehicle(true);
         $loan->setReturnKey(true);
         $loan->setReturnDate(new \DateTime());
@@ -110,7 +139,7 @@ class LoanController extends AbstractController
         $entityManager->persist($loan);
         $entityManager->flush();
         
-        return $this->redirectToRoute('loan_list');
+        return $this->redirectToRoute('loan_gestion');
     }
 
     #[Route('/annuler/{id}', name: 'annuler')]
@@ -120,20 +149,42 @@ class LoanController extends AbstractController
 
         $user = $this->getUser();
         $loan = $loanRepository->find($id);
+        $now = new \DateTime("now");
 
-        if($loan->getDriver()->getId() == $user->getId())
-        {
-            $loan->setStatut("Annulé");
-            $loan->setReturnVehicle(true);
-            $loan->setReturnKey(true);
-            $loan->setReturnDate(new \DateTime());
-            $entityManager->persist($loan);
-            $entityManager->flush();
-            $pathInfo = $request->getPathInfo();
-            $requestUri = $request->getRequestUri();
+        if($loan->getStatut() != "Validé" && $loan->getStatut() != "En validation" ){
+            $this->addFlash(
+                'danger',
+                'La demande d\'emprunt doit être en validation ou validé !'
+            );
+            return $this->redirectToRoute('loan_maListe');
         }
 
-        return $this->redirectToRoute('loan_list');
+        if($now > $loan->getDepartDate() && $loan->getStatut() != "En validation" ){
+            $this->addFlash(
+                'danger',
+                'Trajet en cours ou passé, Impossible d\'annuler!'
+            );
+            return $this->redirectToRoute('loan_maListe');
+        }   
+
+        if(!$loan->getDriver()->getId() == $user->getId())
+        {
+            $this->addFlash(
+                'danger',
+                'Je ne suis pas l\'emprunteur, Impossible d\'annuler!'
+            );
+            return $this->redirectToRoute('loan_maListe');
+        }
+        $loan->setStatut("Annulé");
+        $loan->setReturnVehicle(true);
+        $loan->setReturnKey(true);
+        $loan->setReturnDate(new \DateTime());
+        $entityManager->persist($loan);
+        $entityManager->flush();
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        return $this->redirectToRoute('loan_maListe');
     }
 
     #[Route('/rejoindre/{id}', name: 'rejoindre')]
@@ -143,18 +194,34 @@ class LoanController extends AbstractController
 
         $user = $this->getUser();
         $loan = $loanRepository->find($id);
+        $now = new \DateTime("now");
         
-        if($loan->getStatut() != "Validé" && $loan->getDepartDate() > new \DateTime("now") ){
+        if($loan->getStatut() != "Validé" ){
             $this->addFlash(
                 'danger',
                 'La demande d\'emprunt n\'est pas validé'
             );
             return $this->redirectToRoute('loan_list');
         }
-        if($loan->getDepartDate() < new \DateTime("now") ){
+        if($now > $loan->getDepartDate() ){
             $this->addFlash(
                 'danger',
-                'Trajet en cours, Impossible de rejoindre !'
+                'Trajet en cours ou passé, Impossible de rejoindre !'
+            );
+            return $this->redirectToRoute('loan_list');
+        }
+        if( $loan->userInPassengers($user) ){
+            $this->addFlash(
+                'danger',
+                'Je fais déjà partie du trajet, Impossible de rejoindre !'
+            );
+            return $this->redirectToRoute('loan_list');
+        }
+        if($loan->getDriver()->getId() == $user->getId())
+        {
+            $this->addFlash(
+                'danger',
+                'Je ne suis conducteur, Impossible de rejoindre !'
             );
             return $this->redirectToRoute('loan_list');
         }
@@ -171,18 +238,19 @@ class LoanController extends AbstractController
 
         $user = $this->getUser();
         $loan = $loanRepository->find($id);
-        
-        if($loan->getStatut() != "Validé" && $loan->getDepartDate() > new \DateTime("now") ){
+        $now = new \DateTime("now");
+
+        if( $loan->getStatut() != "Validé" ){
             $this->addFlash(
                 'danger',
                 'La demande d\'emprunt n\'est pas validé'
             );
             return $this->redirectToRoute('loan_list');
         }
-        if($loan->getDepartDate() < new \DateTime("now") ){
+        if( $now > $loan->getDepartDate() ){
             $this->addFlash(
                 'danger',
-                'Trajet en cours, Impossible de rejoindre !'
+                'Trajet en cours ou passé, Impossible de quitter !'
             );
             return $this->redirectToRoute('loan_list');
         }
@@ -199,6 +267,43 @@ class LoanController extends AbstractController
 
         return $this->redirectToRoute('loan_list');
     }
+    #[Route('/removePassenger/{idLoan}/{idUser}', name: 'remove_passenger')]
+    public function removePassenger(int $idLoan,int $idUser, EntityManagerInterface $entityManager, Request $request, LoanRepository $loanRepository, PersonRepository $personRepository ): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $user = $personRepository->find($idUser);
+        $loan = $loanRepository->find($idLoan);
+        $now = new \DateTime("now");
+        
+        if($loan->getStatut() != "Validé"  ){
+            $this->addFlash(
+                'danger',
+                'La demande d\'emprunt n\'est pas validé'
+            );
+            return $this->redirectToRoute('loan_maListe');
+        }
+        if( $now > $loan->getDepartDate() ){
+            $this->addFlash(
+                'danger',
+                'Trajet en cours ou passé, Impossible de retirer le passager !'
+            );
+            return $this->redirectToRoute('loan_maListe');
+        }
+        if(!$loan->userInPassengers($user)){
+            $this->addFlash(
+                'danger',
+                'Utilisateur n\'est pas dans le trajet !'
+            );
+            return $this->redirectToRoute('loan_maListe');
+        }
+        $loan->deletePassanger($user);
+        $entityManager->persist($loan);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('loan_maListe');
+    }
+
     #[Route('/complete/{id}', name: 'complete')]
     public function complete(int $id, EntityManagerInterface $entityManager, Request $request, LoanRepository $loanRepository): Response
     {
@@ -207,17 +312,30 @@ class LoanController extends AbstractController
         $user = $this->getUser();
         $loan = $loanRepository->find($id);
 
-        if($loan->getDriver()->getId() == $user->getId())
-        {
-            $loan->setStatut("Complété");
-            $loan->setReturnDate(new \DateTime());
-            $entityManager->persist($loan);
-            $entityManager->flush();
-            $pathInfo = $request->getPathInfo();
-            $requestUri = $request->getRequestUri();
+        if( new \DateTime("now") < $loan->getReturnDate() ){
+            $this->addFlash(
+                'danger',
+                'Trajet pas terminer, Impossible de terminer le trajet !'
+            );
+            return $this->redirectToRoute('loan_maListe');
         }
 
-        return $this->redirectToRoute('loan_list');
+        if(!$loan->getDriver()->getId() == $user->getId())
+        {
+            $this->addFlash(
+                'danger',
+                'Je ne suis pas le demandeur de cet emprunt, Impossible de terminer le trajet !'
+            );
+            return $this->redirectToRoute('loan_maListe');
+        }
+        
+        $loan->setStatut("Complété");
+        $loan->setReturnDate(new \DateTime());
+        $entityManager->persist($loan);
+        $entityManager->flush();
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+        return $this->redirectToRoute('loan_maListe');
     }
 
     #[Route('/rendrev/{id}', name: 'rendreV')]
@@ -254,6 +372,6 @@ class LoanController extends AbstractController
         $entityManager->persist($loan);
         $entityManager->flush();
 
-        return $this->redirectToRoute('loan_list');
+        return $this->redirectToRoute('loan_gestion');
     }
 }
